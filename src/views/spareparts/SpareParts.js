19600d813d948f617f539b225a2bb508518d5831
Kaiser Souza <93536968+KaiserSouza69420@users.noreply.github.com>
2022-09-10 20:32:37 +0530

    Update SpareParts.js

--- src/views/spareparts/SpareParts.js
+++ src/views/spareparts/SpareParts.js
@@ -1,23 +1,40 @@
-import React, { memo, useEffect } from 'react'
+//remains same
+import React, { memo, useEffect, useState } from 'react'
 import {
   IconButton,
   ButtonToolbar,
   ButtonGroup,
   FlexboxGrid,
   Form,
+  Schema,
   Button,
   Input,
   Modal,
   SelectPicker,
   Uploader,
+  Message,
+  useToaster,
 } from 'rsuite'
 import PlusIcon from '@rsuite/icons/Plus'
 import { Table, Column, HeaderCell, Cell } from 'rsuite-table'
 import 'rsuite-table/dist/css/rsuite-table.css'
 import { faker } from '@faker-js/faker'
-import { db } from '../../firebase'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { getDatabase, ref, set, child, push, update, get, remove } from 'firebase/database'
+import { getAuth } from 'firebase/auth'
+//cell imports
+import {
+  ActionCell,
+  BaseCell,
+  CheckCell,
+  DeleteCell,
+  EditableCell,
+  ImageCell,
+  InputCell,
+} from '../../utils/tableComponents'
+import { auth, createUserWithEmailAndPassword } from 'firebase/auth'
+import { getStorage, ref as storageRe, uploadBytes } from 'firebase/storage'
 
+// change according to your needs
 const selectDataState = ['Goa', 'Karnataka', 'Maharshtra'].map((item) => ({
   label: item,
   value: item,
@@ -38,257 +55,65 @@ const selectDataCountry = ['India', 'USA'].map((item) => ({
   value: item,
 }))
 
+// change form validation according to your needs
+const model = Schema.Model({
+    partName: Schema.Types.StringType().isRequired('This field is required.'),
+    Eng_id: Schema.Types.NumberType().isRequired('This field is required.'),
+})
+// no changes
 const Textarea = React.forwardRef((props, ref) => <Input {...props} as="textarea" ref={ref} />)
 Textarea.displayName = 'Textarea'
 
-const EditableCell = ({ rowData, dataKey, onChange, ...props }) => {
-  const editing = rowData.status === 'EDIT'
-  return (
-    <Cell {...props} className={editing ? 'table-content-editing' : ''}>
-      {editing ? (
-        <input
-          className="rs-input"
-          defaultValue={rowData[dataKey]}
-          onChange={(event) => {
-            onChange && onChange(rowData.id, dataKey, event.target.value)
-          }}
-        />
-      ) : (
-        <span className="table-content-edit-span">{rowData[dataKey]}</span>
-      )}
-    </Cell>
-  )
-}
-
-const BaseCell = React.forwardRef((props, ref) => {
-  const { children, rowData, ...rest } = props
-  return (
-    <Cell
-      ref={ref}
-      rowData={rowData}
-      onDoubleClick={() => {
-        console.log(rowData)
-      }}
-      {...rest}
-    >
-      {children}
-    </Cell>
-  )
-})
-
-BaseCell.displayName = 'BaseCell'
-
-const CheckCell = ({ rowData, onChange, checkedKeys, dataKey, ...props }) => {
-  return (
-    <BaseCell {...props} style={{ padding: 0 }}>
-      <div style={{ lineHeight: '46px' }}>
-        <input
-          type="checkbox"
-          value={rowData[dataKey]}
-          onChange={onChange}
-          checked={checkedKeys.some((item) => item === rowData[dataKey])}
-        />
-      </div>
-    </BaseCell>
-  )
-}
-//edited
-//edit 2
-// const NameCell = ({ rowData, dataKey, ...props }) => {
-//   const Overlay = React.forwardRef(({ style, onClose, ...rest }, ref) => {
-//     const styles = {
-//       ...style,
-//       shadows: '0px 0px 10px rgba(0, 0, 0, 0.5)',
-//       color: '#000',
-//       background: '#fff',
-//       width: 200,
-//       opacity: 1,
-//       padding: 10,
-//       borderRadius: 4,
-//       boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)',
-//       position: 'absolute',
-//       zIndex: 1,
-//       transform: 'translate(0, -20px)',
-//     }
-
-//     return (
-//       <div {...rest} style={styles} ref={ref}>
-//         <p>
-//           <b>Name:</b> {`${rowData.firstName} ${rowData.lastName}`}{' '}
-//         </p>
-//         <p
-//           style={{
-//             display: 'flex',
-//             flexWrap: 'wrap',
-//           }}
-//         >
-//           <b>District:</b> {rowData.district}{' '}
-//         </p>
-//         <p>
-//           <b>City:</b> {rowData.city}{' '}
-//         </p>
-//         <p>
-//           <b>State:</b> {rowData.state}{' '}
-//         </p>
-//         <p>
-//           <b>Country:</b> {rowData.country}{' '}
-//         </p>
-//       </div>
-//     )
-//   })
-//   Overlay.displayName = 'Overlay'
-//   const speaker = (props, ref) => {
-//     const { className, top, onClose } = props
-//     return (
-//       <Overlay
-//         title="Description"
-//         style={{ top }}
-//         onClose={onClose}
-//         className={className}
-//         ref={ref}
-//         visible
-//       />
-//     )
-//   }
-
-//   return (
-//     <BaseCell rowData={rowData} {...props}>
-//       <Whisper trigger="click" placement="auto" speaker={speaker} enterable>
-//         <Button
-//           style={{
-//             background: '#fff',
-//             color: 'blue',
-//             border: '0px solid #000',
-//             lineHeight: '50px',
-//             textAlign: 'center',
-//             verticalAlign: 'middle',
-//             marginRight: '10px',
-//           }}
-//         >
-//           {rowData[dataKey].toLocaleString()}
-//         </Button>
-//       </Whisper>
-//     </BaseCell>
-//   )
-// }
-
-const ActionCell = ({ rowData, dataKey, onClick, ...props }) => {
-  return (
-    <Cell {...props} style={{ padding: '6px' }}>
-      <Button
-        appearance="link"
-        onClick={() => {
-          onClick(rowData.id)
-        }}
-      >
-        {rowData.status === 'EDIT' ? 'Save' : 'Edit'}
-      </Button>
-    </Cell>
-  )
-}
-
-const DeleteCell = ({ rowData, dataKey, onClick, ...props }) => {
-  return (
-    <Cell {...props} style={{ padding: '6px' }}>
-      <Button
-        appearance="link"
-        onClick={() => {
-          onClick(rowData.id)
-        }}
-      >
-        {'Delete'}
-      </Button>
-    </Cell>
-  )
-}
-
-const InputCell = memo(({ rowData, data, value, onChange, ...props }) => {
-  function handleChange(event) {
-    onChange(rowData.id, event.target.value)
-  }
-
-  return (
-    <BaseCell {...props}>
-      <input value={data[rowData.id]} onChange={handleChange} />
-    </BaseCell>
-  )
-})
-
-InputCell.displayName = 'InputCell'
-
-//change this
-function createRows() {
-  const rows = []
-
-  for (let i = 0; i < 50; i++) {
-    const parts = {
-      id: i,
-      avatar: faker.image.avatar(),
-      partName: faker.commerce.product(),
-      Eng_id: i,
-    }
-    rows.push(parts)
-  }
-
-  // const users = await db
-  //   .collection('user')
-  //   .get()
-  //   .then((querySnapshot) => {
-  //     querySnapshot.docs.map((doc) => {
-  //       rows.push(doc.data())
-  //       return doc.data()
-  //     })
-  //   })
-  // console.log('LOG 2', users)
-  // console.log(rows)
-  return rows
-}
-
-// data.map((item) => {
-//   return db
-//     .collection('user')
-//     .doc('one')
-//     .set(item)
-//     .then(() => {
-//       console.log('Document successfully written!')
-//     })
-// })
-
-const ImageCell = ({ rowData, dataKey, ...rest }) => (
-  <Cell {...rest}>
-    <img
-      src={rowData[dataKey]}
-      width="50"
-      alt="avtar"
-      style={{
-        borderRadius: '50%',
-        verticalAlign: 'middle',
-        marginRight: '10px',
-      }}
-    />
-  </Cell>
+const TextField = ({ cid, name, label, accepter, ...rest }) => (
+  <Form.Group controlId={cid}>
+    <Form.ControlLabel>{label}</Form.ControlLabel>
+    <Form.Control name={name} accepter={accepter} {...rest} />
+  </Form.Group>
 )
 
 const SpareParts = () => {
+  //table states
   const [checkedKeys, setCheckedKeys] = React.useState([])
   const [sortColumn, setSortColumn] = React.useState()
   const [sortType, setSortType] = React.useState()
   const [loading, setLoading] = React.useState(false)
-  const [data, setData] = React.useState(createRows())
-  // useState for add user
+  const [data, setData] = React.useState([])
+  //delete states
+  const [deleteUserModal, setDeleteUserModal] = React.useState(false)
+  const handleCloseDeleteModal = () => setDeleteUserModal(false)
+  const [deleteId, setDeleteId] = React.useState()
+  // add states
   const [open, setOpen] = React.useState(false)
+  const formRef = React.useRef()
+  ///change
   const [formValue, setFormValue] = React.useState({
+    avatar: 'https://www.gravatar.com/avatar/0?d=mp&f=y',
     partName: '',
     Eng_id: '',
   })
+  const toaster = useToaster()
+  const message = (
+    <Message showIcon type="success">
+      Spare Part Added Successfully
+    </Message>
+  )
+  useEffect(() => {
+    if (formValue.avatar[0].blobFile) {
+      const file = formValue.avatar[0].blobFile
+      const storage = getStorage()
+      const storageRef = storageRe(storage, `/userAvatars/${file.name}`)
+      uploadBytes(storageRef, file)
+        .then((snapshot) => {
+          console.log('Uploaded a blob or file!', snapshot.ref.fullPath)
+          console.log('url', snapshot.ref.name)
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+    }
+  }, [formValue])
 
-  const handleClose = () => {
-    setOpen(false)
-  }
-  const handleOpen = () => {
-    setOpen(true)
-  }
-
+  // table functions
   const getData = () => {
     if (sortColumn && sortType) {
       return data.sort((a, b) => {
@@ -334,23 +159,166 @@ const SpareParts = () => {
     },
     [checkedKeys],
   )
-  //change this
-  const handleChange = (id, key, value) => {
-    const nextData = Object.assign([], data)
-    nextData.find((item) => item.id === id)[key] = value
-    setData(nextData)
-  }
+
   const handleEditState = (id) => {
     const nextData = Object.assign([], data)
     const activeItem = nextData.find((item) => item.id === id)
     activeItem.status = activeItem.status ? null : 'EDIT'
     setData(nextData)
   }
+  // end of table functions
 
-  //change this
+  // posting data to firebase
+  // make changes
+  const addDataToFirebase = (data) => {
+    if (!formRef.current.check()) {
+      console.error('Form Error')
+      return
+    }
+    // only add this
+    const db = getDatabase()
+    set(ref(db, 'spareparts/spare/' + uid), { id: uid, ...formValue }).then(() => {
+    console.log('Data saved!')
+    handleClose()
+    })
+    // only
+    const auth = getAuth()
+    createUserWithEmailAndPassword(auth, formValue.email, formValue.password)
+      .then((userCredential) => {
+        const user = userCredential.user
+        const uid = user.uid
+        const db = getDatabase()
+
+        if (formValue.avatar[0].blobFile) {
+          const file = formValue.avatar[0].blobFile
+          const storage = getStorage()
+          const storageRef = storageRe(storage, `/userAvatars/${uid}`)
+          uploadBytes(storageRef, file)
+            .then((snapshot) => {
+              getDownloadURL(storageRe(storage, snapshot.ref.fullPath))
+                .then((downloadURL) => {
+                  setFormValue({ ...formValue, avatarUrl: downloadURL })
+                  return downloadURL
+                })
+                .then((downloadURL) => {
+                  set(ref(db, 'spareparts/spare/' + uid), {
+                    id: uid,
+                    ...formValue,
+                    avatarUrl: downloadURL,
+                  }).then(() => {
+                    const nextData = getData()
+                    setData([...nextData, { id: uid, ...formValue, avatarUrl: downloadURL }])
+                    handleClose()
+                    setMessageVal({ message: 'Spare added successfully', type: 'success' })
+                    toaster.push(message, 'topCenter')
+                    setFormValue({
+                      avatar: null,
+                      partName:'',
+                      Eng_id:'',
+                      avatarUrl: 'https://www.gravatar.com/avatar/0?d=mp&f=y',
+                    })
+                  })
+                })
+            })
+            .catch((e) => {
+              setMessageVal({ message: e.message, type: 'error' })
+              toaster.push(message, 'topCenter')
+            })
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        if (errorCode === 'auth/email-already-in-use') {
+          setMessageVal({ message: 'Email already in use', type: 'error' })
+          toaster.push(message, 'topCenter')
+        } else if (errorCode === 'auth/invalid-email') {
+          setMessageVal({ message: 'Invalid email', type: 'error' })
+          toaster.push(message, 'topCenter')
+        }
+      })
+  }
+
+  // setting states for delete
+  const handleShowDeleteModal = (id) => {
+    setDeleteUserModal(true)
+    setDeleteId(id)
+  }
+
+  // handle states for add
+  const handleClose = () => {
+    setOpen(false)
+  }
+  const handleOpen = () => {
+    setOpen(true)
+  }
+  // ;(() => {
+  //   const rows = []
+  //   for (let i = 0; i < 100; i++) {
+  //     const user = {
+  //       id: i,
+  //       avatar: faker.image.avatar(),
+  //       firstName: faker.name.firstName(),
+  //       lastName: faker.name.lastName(),
+  //       email: faker.internet.exampleEmail(),
+  //       contactNumber: faker.phone.number(),
+  //       add_1: faker.address.streetAddress(),
+  //       add_2: faker.address.secondaryAddress(),
+  //       pincode: faker.address.zipCode(),
+  //       district: faker.address.city(),
+  //       city: faker.address.city(),
+  //       state: faker.address.state(),
+  //       country: faker.address.country(),
+  //       sentence: faker.lorem.sentence(),
+  //     }
+  //     rows.push(user)
+  //   }
+  //   const db = getDatabase()
+  //   set(ref(db, 'users/customers/'), rows)
+  // })()
+  ///
+  //
+  //make changes - retirve data from firebase
+
+  useEffect(() => {
+    const dbRef = ref(getDatabase())
+    // changew only this
+    get(child(dbRef, `spareparts/spare`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          setData(Object.values(snapshot.val()))
+        } else {
+          console.log('No data available')
+        }
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }, [])
+
+  //change this - update data in firebase
+  const handleChange = (id, key, value) => {
+    // db.collection('user')
+    //   .doc(id)
+    //   .update({ [key]: value })
+    const nextData = Object.assign([], data)
+    nextData.find((item) => item.id === id)[key] = value
+    setData(nextData)
+    const db = getDatabase()
+    // changew only this
+    update(ref(db, 'spareparts/spare/' + id), {
+      [key]: value,
+    })
+  }
+  //change this - delete from firebase
   const handleDeleteState = (id) => {
+    // delete data[id]
+    //
+    const db = getDatabase()
+    // changew only this
+    remove(ref(db, 'spareparts/spare/' + id))
     setData(data.filter((item) => item.id !== id))
   }
+
   return (
     <>
       {/* add new user button */}
@@ -359,25 +327,24 @@ const SpareParts = () => {
           <Modal.Title>New Spare</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form fluid onChange={setFormValue} formValue={formValue}>
-            <Form.Group controlId="uploader">
-              <Form.ControlLabel>Spare Picture:</Form.ControlLabel>
-              <Form.Control name="uploader" accepter={Uploader} action="#" />
-            </Form.Group>
-            <Form.Group controlId="partName-9">
-              <Form.ControlLabel>Spare Name</Form.ControlLabel>
-              <Form.Control name="partName" />
-              <Form.HelpText>Required</Form.HelpText>
-            </Form.Group>
-            <Form.Group controlId="Eng_id-9">
-              <Form.ControlLabel>Engine Name</Form.ControlLabel>
-              <Form.Control name="Eng_id" />
-              <Form.HelpText>Required</Form.HelpText>
-            </Form.Group>
+          <Form fluid ref={formRef} model={model} onChange={setFormValue} formValue={formValue}>
+            <TextField
+              cid="avatar"
+              name="avatar"
+              label="Part Picture"
+              accepter={Uploader}
+              // action=""
+            />
+            <TextField cid="partName-9" name="partName" label="part Name" />
+            <TextField cid="Eng_id-9" name="Eng_id" label="Engine Id" />
+            {/* <Form.Group controlId="textarea-9">
+              <Form.ControlLabel>Textarea</Form.ControlLabel>
+              <Form.Control rows={5} name="textarea" accepter={Textarea} />
+            </Form.Group> */}
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={handleClose} appearance="primary">
+          <Button onClick={addDataToFirebase} appearance="primary" type="submit">
             Confirm
           </Button>
           <Button onClick={handleClose} appearance="subtle">
@@ -406,12 +373,9 @@ const SpareParts = () => {
         headerHeight={50}
         bordered
         cellBordered
-        onRowClick={(data) => {
-          console.log(data)
-        }}
         affixHorizontalScrollbar
       >
-        <Column width={50} align="center" sortable>
+        <Column width={50} align="center" sortable fixed>
           <HeaderCell style={{ padding: 0 }}>
             <div style={{ lineHeight: '40px' }}>
               <input
@@ -424,21 +388,21 @@ const SpareParts = () => {
           <CheckCell dataKey="id" checkedKeys={checkedKeys} onChange={handleCheck} />
         </Column>
 
-        <Column width={70} align="center" fixed sortable>
+        <Column width={70} fixed sortable>
           <HeaderCell>Id</HeaderCell>
           <Cell dataKey="id" />
         </Column>
         <Column width={130} fixed>
-          <HeaderCell>Picture</HeaderCell>
+          <HeaderCell>Avatar</HeaderCell>
           <ImageCell dataKey="avatar" />
         </Column>
         <Column width={100} sortable>
-          <HeaderCell>Spare Name</HeaderCell>
-          <Cell dataKey="partName" />
+          <HeaderCell>Part Name</HeaderCell>
+          <EditableCell dataKey="partName" />
         </Column>
-        <Column width={70} align="center" fixed sortable>
-          <HeaderCell>Engine Id</HeaderCell>
-          <Cell dataKey="Eng_id" />
+        <Column width={100} sortable>
+          <HeaderCell>Engine ID</HeaderCell>
+          <EditableCell dataKey="Eng_id" />
         </Column>
         <Column width={200}>
           <HeaderCell>Edit</HeaderCell>
@@ -446,9 +410,31 @@ const SpareParts = () => {
         </Column>
         <Column width={200}>
           <HeaderCell>Delete</HeaderCell>
-          <DeleteCell dataKey="id" onClick={handleDeleteState} />
+          <DeleteCell dataKey="id" onClick={handleShowDeleteModal} />
         </Column>
       </Table>
+      {/* // no changes */}
+      {/* Delete Modal */}
+      <Modal open={deleteUserModal} onClose={handleCloseDeleteModal}>
+        <Modal.Header>
+          <Modal.Title>Delete User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete this user?</Modal.Body>
+        <Modal.Footer>
+          <Button
+            onClick={() => {
+              handleDeleteState(deleteId)
+              handleCloseDeleteModal()
+            }}
+            appearance="primary"
+          >
+            Confirm
+          </Button>
+          <Button onClick={handleCloseDeleteModal} appearance="subtle">
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   )
 }
