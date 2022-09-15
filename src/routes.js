d9e75e86b9885cac2994eb63d9627d95bc57a85a
Carson <rodriguescarson@hotmail.com>
2022-09-15 12:24:02 +0530

    fix

--- src/routes.js
+++ src/routes.js
@@ -7,14 +7,8 @@ const Admin = React.lazy(() => import('./views/admin/Admin'))
 const Customer = React.lazy(() => import('./views/customer/Customer'))
 const ServiceProvider = React.lazy(() => import('./views/serviceprovider/ServiceProvider'))
 const SpareParts = React.lazy(() => import('./views/spareparts/SpareParts'))
-<<<<<<< HEAD
-
-=======
-const Engines = React.lazy(() => import('./views/engines/Engines'))
-const ServiceSchedule = React.lazy(() => import('./views/serviceschedule/ServiceShedule'))
->>>>>>> 1f94e1036e5bd7007b84c7821d3078bff47be5bd
 const Faq = React.lazy(() => import('./views/faq/Faq'))
-
+const ServiceSchedule = React.lazy(() => import('./views/serviceschedule/ServiceShedule'))
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/home', name: 'Home', element: Home },
